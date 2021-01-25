4f35a4dc4de13424d3901b2f1b433673b489e5c2
mchini <mattia.chini@zmnh.uni-hamburg.de>
2021-01-25 12:05:22 +0100

    comment MI

--- Basic functions/getMI.m
+++ Basic functions/getMI.m
@@ -1,28 +1,21 @@
-function [MI, rows] = getMI(values1, values2, negative)
+function [MI, rows] = getMI(values1, values2)
 % by Mattia, 09/20
-% simple helper function to compute modulation index
+% simple helper function to compute modulation index (normalize "paired" values in the -1 to 1 range)
 % inputs: - values1 & 2 -> vector with values to use to compute MI
-%         - negatives   -> 1 to take out negative values, 0 not to do it
 % output: modulation index for the given values
+
+% important: - if the inputs take negative AND positive values, 
+%			   the behavior of the function is erratic (i.e. 
+%			   normalization between -1 and 1 fails).
+%			 - if the inputs take negative OR positive values, 
+%			   the normalization succeeds
 
 % take only real values if input is complex
 if ~isreal(values1) || ~isreal(values2)
     values1 = real(values1);
     values2 = real(values2);
 end
-% take out mismatch between positive & negatives if it's needed
-if negative > 0
-    all_values = cat(2, values1, values2);
-    % find negative values
-    [rows, ~] = find(all_values < 0);
-    % count how many times per row the value is negative (this is because
-    % if both are negative, than you can keep that row)
-    count = histcounts(rows, 1 : size(all_values, 1));
-    % find rows where there is one negative (and the other is positive)
-    to_delete = find(count == 1);
-    values1(to_delete) = NaN;
-    values2(to_delete) = NaN;
-end
+
 % compute MI
 MI = (values1 - values2) ./ (values1 + values2);
 end
