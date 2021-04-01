9503745e618d4c006e1bb2d6cd74de44dd05c9c8
mchini <37143108+mchini@users.noreply.github.com>
2021-04-01 16:04:02 +0200

    inf theory stuff

---
+++ External Toolboxes/InfTheory/entropy.m
@@ -0,0 +1,16 @@
+function z = entropy(x)
+% Compute entropy z=H(x) of a discrete variable x.
+% Input:
+%   x: a integer vectors  
+% Output:
+%   z: entropy z=H(x)
+% Written by Mo Chen (sth4nth@gmail.com).
+if ~isempty(x)
+    n = numel(x);
+    [~,~,x] = unique(x);
+    Px = accumarray(x, 1)/n;
+    Hx = -dot(Px,log2(Px));
+    z = max(0,Hx);
+else
+    z = NaN;
+end
