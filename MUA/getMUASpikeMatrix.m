589f3bfaf383965b98c59c05a0c2f451225338cb
mchini <mattia.chini@zmnh.uni-hamburg.de>
2021-04-15 09:36:42 +0200

    description_MUAspike_matrix

--- MUA/getMUASpikeMatrix.m
+++ MUA/getMUASpikeMatrix.m
@@ -2,8 +2,22 @@ function spike_matrix = getMUASpikeMatrix(animal_name, ...
     resultsMUA, output_folder, save_data, repeatCalc, area)
 % by Mattia 03.21
 
-% computes spike_matrix starting from the output of detected MUA
-% timestamps (where no SUA is available)
+% computes spike_matrix with 1ms resolution, starting from the output of 
+% detected MUA timestamps (e.g. when no SUA is available/needed)
+
+% if you would like to have all your MUA spikes put into the same spike
+% matrix, then you should combine the MUA structure that is outputted by
+% getMUAspiketimes on a channel-by-channel basis, into one big structure,
+% where every entry of the structure corresponds to one channel
+
+% simple example to achieve what I mention here above
+% for channel = 1 : 16 % assuming your probe has 16 channels, and you want to combine all of them
+%     [timestamps, ~] = getMUAspiketimes(x0,x1,x2,x3);
+%     MUA(channel).timestamps = timestamps;
+% end
+% the MUA that you obtain in this manner can then be fed to this function
+% (it is what you should load on the line that has the comment "load the
+% saved MUA stuff"
 
 % input:  animal_name (string, also known as animal_ID)
 %         resultsMUA (string, folder in which you have detected MUA)
